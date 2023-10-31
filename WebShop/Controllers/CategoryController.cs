f93aae12df3107ba97e679ffd1e2bd8256f33fa0
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-10-31 09:00:13 +0100

    added category list to Category index

--- WebShop/Controllers/CategoryController.cs
+++ WebShop/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebShop.Data;
 
 namespace WebShop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var objCatergoryList = _db.Categories.ToList();
+            return View(objCatergoryList);
         }
     }
 }
