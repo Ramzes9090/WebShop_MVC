927ffef683199537d2a2667a58d32775db06f6d5
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-10-31 08:46:00 +0100

    added controler and some controler object to db

---
+++ WebShop/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
