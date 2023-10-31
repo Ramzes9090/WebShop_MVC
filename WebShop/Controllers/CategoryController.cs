b152c0742baf07f3295b36d73baeb6ca113cc060
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-10-31 13:09:45 +0100

    added validations to category create

--- WebShop/Controllers/CategoryController.cs
+++ WebShop/Controllers/CategoryController.cs
@@ -23,9 +23,18 @@ namespace WebShop.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","The Display Order cannot mach the Category Name.");
+            }
+            if(ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+            
         }
     }
 }
