6e812ca76808a7ebc88b2ef86a11da90fc244db6
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-11-03 13:51:46 +0100

    added model for product and config product for repository

---
+++ WebShop.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.DataAccess.Data;
+using WebShop.DataAccess.Repository.IRepository;
+using WebShop.Models;
+
+namespace WebShop.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository 
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
