3900a03935aabea6f2bd907f73c297378d1e03b6
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-11-04 14:02:30 +0100

    add starting detaling home page

--- WebShop/Areas/Customer/Controllers/HomeController.cs
+++ WebShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebShop.DataAccess.Repository.IRepository;
 using WebShop.Models;
 
 namespace WebShop.Areas.Customer.Controllers
@@ -8,15 +9,18 @@ namespace WebShop.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;   
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
