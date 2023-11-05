74acbd81f509f86c34d482ca3339000d0235b4c9
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-11-05 13:39:15 +0100

    adding details page and some config to add identity in the future

--- WebShop.DataAccess/Data/ApplicationDbContext.cs
+++ WebShop.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebShop.Models;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace WebShop.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
             options) : base(options)
@@ -16,6 +17,8 @@ namespace WebShop.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name="Action",DisplayOrder = 1},
                 new Category { Id = 2, Name="SciFi",DisplayOrder = 2},
